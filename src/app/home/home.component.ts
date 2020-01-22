0537bf72b65b2337d81bb238694f6770ceaf5efd
longhaine <longhaipro9@gmail.com>
2020-01-22 20:43:51 +0700

    update: manual opacity transition carousel section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,8 +8,10 @@ import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig]
 })
 export class HomeComponent implements OnInit {
-  private img = globals.server+"/img";
+  img = globals.server+"/img";  
   showNavigationArrows = true;
+  transition:boolean = false;
+  //carousel init
   cap1 = "\"Sleek, comfortable and I feel so powerful in them.\"";
   cap2 = "\"Really love the cut. Perfect length. Good amount of strecth.\"";
   cap3 = "\"I'm in love with this amazing sweater. Super soft, warm and stylish.\"";
@@ -21,11 +23,41 @@ export class HomeComponent implements OnInit {
     {"image": this.img2, "caption":this.cap2,"by":"KT C"},
     {"image": this.img3, "caption":this.cap3,"by":"Sue"}
   ]
-  captions = [this.cap1,this.cap2,this.cap3];
+
+  //carousel animation
+  carouselAnimation:Element;// carousel section trigger Animation
+  caroulselSection:Element; // the whole section of carousel
+  carouselPrevBtn:Element; //pre button
+  carouselNextBtn:Element; // next button
+  
+
   constructor(config: NgbCarouselConfig) {
   }
-
+  carouselOnInit(){
+    this.caroulselSection = document.getElementById("carousel");
+    let carouselBtns:HTMLCollectionOf<Element> = this.caroulselSection.getElementsByTagName("a");
+    for(var i = 0; i < carouselBtns.length; i++){
+      if(carouselBtns.item(i).classList.contains("carousel-control-prev")){
+        this.carouselPrevBtn = carouselBtns.item(i);
+      }else if(carouselBtns.item(i).classList.contains("carousel-control-next")){
+        this.carouselNextBtn = carouselBtns.item(i);
+      }
+    }
+    if(this.carouselPrevBtn && this.carouselNextBtn){
+      this.carouselPrevBtn.addEventListener('click',this.carouselAnimationOnClick.bind(this));
+      this.carouselNextBtn.addEventListener('click',this.carouselAnimationOnClick.bind(this));
+    }
+  }
+  carouselAnimationOnClick(){
+      this.caroulselSection.setAttribute("style","opacity:0.8");
+      setTimeout(() => {
+      this.caroulselSection.setAttribute("style","opacity:1");
+      this.transition = false;  
+      }, 300);
+  }
   ngOnInit() {
   }
-
+  ngAfterViewInit(){
+    this.carouselOnInit();
+  }
 }
