784ad648728fdf821911458225cc9536982969ae
longhaine <longhaipro9@gmail.com>
2020-03-29 00:25:02 +0700

    update: mobile header UI

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataService } from '../service/data.service'; 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-  constructor() { }
+  userName:string = null;
+  constructor(private dataService:DataService) { }
   openDropdown(target:Element){
     let parentTarget = target.parentElement;
     let status = target.children.item(1);
@@ -22,5 +23,6 @@ export class FooterComponent implements OnInit {
     }
   }
   ngOnInit() {
+    this.userName = this.dataService.getEmailCookie();
   }
 }
